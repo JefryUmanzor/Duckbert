dd27d8d06c56304008a5bc9215d47b646b73afb8
JefryUmanzor <jumanzor9001@gmail.com>
2024-03-03 21:25:18 -0500

    Added Feather Particles for Flying Action

--- Scripts/PlayerFlapAction.gd
+++ Scripts/PlayerFlapAction.gd
@@ -7,6 +7,7 @@ extends PlayerAction
 var player_max_v_speed = 300;
 
 @onready var m_player = $"../.."
+const FLAP_PARTICLE = preload("res://Packed Scenes/Particles/Flap Particle.tscn")
 
 func _activate(player : Player):
 	if not player.is_on_spring:
@@ -14,6 +15,7 @@ func _activate(player : Player):
 		player.velocity.y = jump_strength;
 		player.anim.play_jump();
 		player.anim.play_flap();
+		create_flap_particle()
 
 func _on_enable_action():
 	super._on_enable_action();
@@ -25,6 +27,11 @@ func _on_disable_action():
 	super._on_disable_action();
 	m_player.gravity_scale = 1.0;
 	m_player.max_vertical_speed = player_max_v_speed;
+
+func create_flap_particle():
+	var particle = FLAP_PARTICLE.instantiate();
+	get_tree().current_scene.get_node("Pausable").add_child(particle);
+	particle.global_position = m_player.global_position - Vector2(0.0, 8.0);
 
 func _process(_delta):
 	if active:
