9bfa1d6528d1d7d4999e317e79e81945765d5dc1
JefryUmanzor <jumanzor9001@gmail.com>
2024-03-08 19:49:41 -0500

    Level Clear and Main Menu button navigation working

--- Scripts/GUI/MainMenuScript.gd
+++ Scripts/GUI/MainMenuScript.gd
@@ -3,8 +3,19 @@ extends Control
 @onready var animation_tree = $AnimationTree
 const TEST_ROOM = preload("res://Rooms/Test Room.tscn")
 
+var focused = false;
+@onready var start_button = $VBoxContainer/Start
+
 func _ready():
 	animation_tree.set("parameters/Main Shot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE);
+
+func _process(_delta):
+	if not focused:
+		if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_down"):
+			start_button.grab_focus();
+
+func on_grab_focus():
+	focused = true;
 
 func send_to_test_room():
 	get_tree().change_scene_to_packed(TEST_ROOM);
--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -6,8 +6,13 @@ extends CanvasLayer
 @onready var next_level = $"Base/Level Clear Holder/VBoxContainer/HBoxContainer/Next Level"
 @onready var level_select = $"Base/Level Clear Holder/VBoxContainer/HBoxContainer/Level Select"
 
+var level_cleared = false;
+var focused = false;
+
 func level_clear():
 	animation_tree.set("parameters/Level Clear Shot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE);
 	animation_tree.set("parameters/UI Blend/blend_amount", 1.0);
 	
+	level_cleared = true;
 	next_level.grab_focus();
+	focused = true;
