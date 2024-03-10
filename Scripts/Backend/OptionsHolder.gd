cdd667c12a6b7af914d52f53430fc48a5e705b77
JefryUmanzor <jumanzor9001@gmail.com>
2024-03-10 15:35:03 -0400

    Sets window size on startup

--- Scripts/Backend/OptionsHolder.gd
+++ Scripts/Backend/OptionsHolder.gd
@@ -4,10 +4,35 @@ extends Node
 var options_save : OptionsData;
 
 var options_loaded : bool = false;
+var max_scale : int = 2;
+
+const BASE_WIDTH : int = 256;
+const BASE_HEIGHT : int = 224;
 
 func _ready():
+	get_max_scale();
 	load_options();
 	apply_options();
+
+func get_max_scale():
+	max_scale = 1;
+	
+	var screen_resolution = DisplayServer.screen_get_size();
+	var max_res = min(screen_resolution.x / (BASE_WIDTH as float), screen_resolution.y / (BASE_HEIGHT as float));
+	
+	max_scale = get_closest_power_of_two(max_res);
+	
+	print(max_scale);
+
+func get_closest_power_of_two(val : float) -> int:
+	var n : int = ceili(val);
+	var found = false;
+	
+	while not found:
+		found = ceil(log(n) / log(2)) == floor(log(n) / log(2));
+		if not found:
+			n -= 1;
+	return n;
 
 func save_options():
 	var new_save = FileAccess.open("user://options.save", FileAccess.WRITE);
@@ -43,7 +68,13 @@ func load_options():
 	options_save.options = parse_result;
 
 func apply_options():
+	var window_size : Vector2i = Vector2i(BASE_WIDTH * max_scale, BASE_HEIGHT * max_scale);
+	DisplayServer.window_set_size(window_size);
+	var size : Vector2i = DisplayServer.screen_get_size(0) / 2;
+	DisplayServer.window_set_position((DisplayServer.screen_get_position(0) + size)  - (window_size / 2));
+	
 	var fullscreen : bool = options_save.options.is_fullscreen;
+	
 	if fullscreen:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN);
 	else:
