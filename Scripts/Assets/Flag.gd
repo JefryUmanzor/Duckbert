fbc6e1d4ad26538e04157a54b484f44a74c3ef25
JefryUmanzor <jumanzor9001@gmail.com>
2024-03-11 12:32:40 -0400

    Added flag teleport option for ending

--- Scripts/Assets/Flag.gd
+++ Scripts/Assets/Flag.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@export var teleport_player : bool = false;
+@export var teleport_offset : Vector2 = Vector2(0.0, -512);
+
 func on_touch_player(_player):
 	get_tree().current_scene.get_node("GUI").level_clear();
-	(_player as Player).can_control = false;
+	var player : Player = (_player as Player);
+	player.can_control = false;
+	
+	if teleport_player:
+		player.gravity_scale = 0.0;
+		player.velocity = Vector2.ZERO;
+		player.global_position = global_position + teleport_offset;
