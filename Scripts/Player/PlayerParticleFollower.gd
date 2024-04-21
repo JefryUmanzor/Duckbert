d49da29b8c5a6d2140d21997ca1a8cc58acf7b10
JefryUmanzor <jumanzor9001@gmail.com>
2024-04-20 22:50:15 -0400

    Added particles to main menu scene for webgl

--- Scripts/Player/PlayerParticleFollower.gd
+++ Scripts/Player/PlayerParticleFollower.gd
@@ -1,11 +1,14 @@
 extends GPUParticles2D
 
 var player : Player;
+const DUCKBERT_SPRITE = preload("res://Sprites/Duckbert Sprite.png")
 
 func _ready():
 	player = get_parent().get_parent().get_parent().get_parent() as Player;
 
 func _process(_delta):
-	texture = player.char_sprite.texture;
-	scale.x = -1 if player.char_sprite.flip_h else 1;
-	scale.y = -1 if player.char_sprite.flip_v else 1;
+	if player != null:
+		texture = player.char_sprite.texture;
+		
+		scale.x = -1 if player.char_sprite.flip_h else 1;
+		scale.y = -1 if player.char_sprite.flip_v else 1;
